f1a208650bae8fe80c89da70c17d38dc96e8bc7e
mickaeldamathadev <127761598+mickaeldamathadev@users.noreply.github.com>
2023-05-30 08:03:37 +0200

    rebuild sms to class

--- lib/sms.d.ts
+++ lib/sms.d.ts
@@ -1,50 +1,8 @@
-/**
-
-    Interface representing the Twilio configuration.
-    @interface ITwilio
-    @property {string} accountSid - The Twilio account SID.
-    @property {string} authToken - The Twilio authentication token.
-    @property {string} sender - The phone number or alphanumeric sender ID.
-
-    */
-interface ITwilio {
-    accountSid: string;
-    authToken: string;
-    sender: string;
+export default class Sms {
+    static init(props: {
+        accountSid: string;
+        authToken: string;
+        sender: string;
+    }): void;
+    static send(body: string, to: string): Promise<import("twilio/lib/rest/api/v2010/account/message").MessageInstance>;
 }
-/**
-
-    Interface representing an SMS message.
-    @interface ISMS
-    @property {ITwilio} twilio - The Twilio configuration.
-    @property {string} dest - The destination phone number.
-    @property {string} msg - The message content.
-    */
-interface ISMS {
-    twilio: ITwilio;
-    dest: string;
-    msg: string;
-}
-/**
-
-    Sends an SMS message using the Twilio API.
-    @async
-    @param {ISMS} props - The SMS properties.
-
-    Interface representing an SMS message.
-    @interface ISMS
-    @property {ITwilio} twilio - The Twilio configuration.
-    @property {string} dest - The destination phone number.
-    @property {string} msg - The message content.
-
-    Interface representing the Twilio configuration.
-    @interface ITwilio
-    @property {string} accountSid - The Twilio account SID.
-    @property {string} authToken - The Twilio authentication token.
-    @property {string} sender - The phone number or alphanumeric sender ID.
-
-
-    @returns {Promise<object|Error>} - A promise that resolves to the created SMS message object or an error.
-    */
-export default function sms(props: ISMS): Promise<object | unknown>;
-export {};
